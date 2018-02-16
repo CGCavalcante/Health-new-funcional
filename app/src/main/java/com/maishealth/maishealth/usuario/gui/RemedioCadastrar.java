fa404ecb954829e09f8fea2115116556647c81a6
Eliana Maria <elianamsfcontatos@gmail.com>
2018-02-16 00:04:02 -0300

    onClick Inserir Medicamento

--- app/src/main/java/com/maishealth/maishealth/usuario/gui/RemedioCadastrar.java
+++ app/src/main/java/com/maishealth/maishealth/usuario/gui/RemedioCadastrar.java
@@ -4,15 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.maishealth.maishealth.R;
+import com.maishealth.maishealth.infra.GuiUtil;
+import com.maishealth.maishealth.usuario.negocio.ServicosMedicamento;
+import com.maishealth.maishealth.usuario.negocio.ValidaCadastro;
 
 public class RemedioCadastrar extends AppCompatActivity {
+    private EditText nomeRemedio, fornecedor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remedio_cadastrar);
+
     }
 
     private void mudarTela(Class tela) {
@@ -29,4 +35,50 @@ public class RemedioCadastrar extends AppCompatActivity {
     public void sairMenuRemedio(View view) {
         this.mudarTela(RemedioMenu.class);
     }
+
+    public void onClickInserirMedicamento(View view){
+        boolean valido = true;
+
+        ValidaCadastro validaCadastro = new ValidaCadastro();
+
+        nomeRemedio = findViewById(R.id.nomeMedicamento);
+        fornecedor = findViewById(R.id.fornecedorMedicamento);
+
+        String nomeRemedioString = nomeRemedio.getText().toString();
+        String fornecedorString = fornecedor.getText().toString();
+
+        if (validaCadastro.isCampoVazio(nomeRemedioString)  && validaCadastro.isCampoVazio(fornecedorString))
+        {
+            nomeRemedio.requestFocus();
+            nomeRemedio.setError("Campo obrigatório!");
+            fornecedor.requestFocus();
+            fornecedor.setError("Campo obrigatório!");
+            valido = false;
+        }
+
+        if (validaCadastro.isCampoVazio(nomeRemedioString)){
+
+            nomeRemedio.requestFocus();
+            nomeRemedio.setError("Campo obrigatório!");
+            valido = false;
+
+        }
+
+        if (validaCadastro.isCampoVazio(fornecedorString)){
+
+            fornecedor.requestFocus();
+            fornecedor.setError("Campo obrigatório!");
+            valido = false;
+
+        }
+
+        if (valido) {
+
+            ServicosMedicamento servicosMedicamento=new ServicosMedicamento(this);
+            servicosMedicamento.cadastrarMedicamento(nomeRemedioString, fornecedorString);
+
+        }
+
+    }
+
 }
